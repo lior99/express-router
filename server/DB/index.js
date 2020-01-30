356fc5a2fdd67a197207cc6609c407f6ea10922a
Lior Shtivelman <lior.shtivelman@gmail.com>
2020-01-30 23:28:47 +0200

    remove console.log

--- server/DB/index.js
+++ server/DB/index.js
@@ -22,7 +22,6 @@ const dbHandler = {
 
     insert: async ({ playerName }) => {
         try {
-            console.log('playerName', playerName);
             const instance = await dbHandler.connect();
             const id = instance.db("players").collection("players").insertOne({ "name": playerName });
             instance.close();
